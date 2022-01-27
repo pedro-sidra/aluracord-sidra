34cba5e1c53bdac2cf4c794274d5311fc0e33a85
Pedro Freitas <pedrosidra0@gmail.com>
2022-01-26 21:15:24 -0300

    Add states, github API and typing timeout
    Add new features from class number 2:
    
    * useStates for the textbox functionality
    * useRouter for pushing to the chat page
    
    * Challenges:
    	* Validate the input string by using length > 2
    	* Get the user`s name from the github API and show a custom
    	welcome
    	* Use a 'timeout' state to only update image and github info
    	500ms after nothing has been typed in the input box.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,36 +1,7 @@
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
 import appConfig from "../config.json";
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
+import React from "react";
+import { useRouter } from "next/router";
 
 function Titulo(props) {
   const Tag = props.tag || "h1";
@@ -48,25 +19,23 @@ function Titulo(props) {
   );
 }
 
-// Componente React
-// function HomePage() {
-//     // JSX
-//     return (
-//         <div>
-//             <GlobalStyle />
-//             <Titulo tag="h2">Boas vindas de volta!</Titulo>
-//             <h2>Discord - Alura Matrix</h2>
-//         </div>
-//     )
-// }
-// export default HomePage
-
 export default function PaginaInicial() {
-  const username = "pedro-sidra";
+  // ======== STATES
+  // Usuario temp. ( sendo digitado )
+  const [tUsername, setTUsername] = React.useState("");
+  // Usuario escolhido (Terminou de digitar)
+  const [username, setUsername] = React.useState("pedro-sidra");
+  // Timeout para a escrita
+  const [typeTimeout, setTypeTimeout] = React.useState("");
+
+  // User info from API
+  const [userInfo, setUserInfo] = React.useState("");
+
+  // Router
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
@@ -104,6 +73,12 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (props) {
+              props.preventDefault();
+              console.log("Submetido:", props);
+              roteamento.push("/chat");
+              // window.location.href="/chat"
+            }}
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -114,7 +89,7 @@ export default function PaginaInicial() {
               marginBottom: "32px",
             }}
           >
-            <Titulo tag="h2">Bem vindo!</Titulo>
+            <Titulo tag="h2">Bem vindo{userInfo.name ? ", " + userInfo.name : ""}!</Titulo>
             <Text
               variant="body3"
               styleSheet={{
@@ -127,6 +102,50 @@ export default function PaginaInicial() {
 
             <TextField
               fullWidth
+              value={tUsername}
+              onChange={function (event) {
+                const valor = event.target.value;
+                console.log("digitou:", valor);
+
+                setTUsername(valor);
+
+                if (typeTimeout) {
+                  clearTimeout(typeTimeout);
+                }
+
+                setTypeTimeout(
+                  setTimeout(() => {
+                    if (tUsername.length > 2) {
+                      fetch("https://api.github.com/users/" + valor)
+                        .then(function (response) {
+                          if (response.ok) {
+                            response.json().then(function (json) {
+                              console.log(json.login);
+
+                              setUsername(valor);
+                              setUserInfo(json);
+                            });
+                          }
+                        })
+                        .catch(function (error) {
+                          console.log(error);
+                        });
+                    }
+                  }, 500)
+                );
+
+                //   .then(function (response) {
+                //     if (response.ok) {
+                //       response.json().then(function (json) {
+                //         setUsername(json.login);
+                //       });
+                //     }
+                //   })
+                //   .catch(function (error) {
+                //     console.log("user does not exist");
+                //   });
+                // setTUsername(valor);
+              }}
               placeholder="Seu usuário aqui"
               textFieldColors={{
                 neutral: {
